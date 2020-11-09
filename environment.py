121e239c98bff7b2d407cb9e44e748f1f36342dd
HYUNGJIN YOON <stargaze221@gmail.com>
2020-11-09 13:14:58 -0800

    fire model update

--- environment.py
+++ environment.py
@@ -1,9 +1,15 @@
 import torch
 from torch.distributions.categorical import Categorical
 from torch import nn
+from torch.autograd import Variable
+
 
 import numpy as np
 import cv2
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -16,11 +22,12 @@ class FireMap:
         self.N_state = 3
 
         ### State Variable ###
-        self.prob_dist = torch.zeros(self.N_state, self.map_width, self.map_height).to(DEVICE)
-        self.prob_dist[0][:][:] = 0.9
-        self.prob_dist[1][:][:] = 0.1
-        self.prob_dist[2][:][:] = 0.01
+        self.init_prob_dist = torch.zeros(self.N_state, self.map_width, self.map_height).to(DEVICE)
+        self.init_prob_dist[0][:][:] = 0.9
+        self.init_prob_dist[1][:][:] = 0.01
+        self.init_prob_dist[2][:][:] = 0.00
 
+        self.realization_state = self.sample_gridmap_from_fire_dist(self.init_prob_dist)
 
         ### State Transition Model ###
         self.transition = nn.Conv2d(3, 3, 3, stride=1, padding=1).to(DEVICE)
@@ -33,92 +40,77 @@ class FireMap:
             if name == 'weight':
                 print(name)
                 param.data = torch.zeros_like(param.data)
-                # Probability to be normal state (output: 0) depends
-                param[0][0][:][:] = torch.Tensor([[0.5, 0.5, 0.5],[0.5, 0.9, 0.5],[0.5, 0.5, 0.5]]) # on the preivous state (0:normal) of the grid and its neighbors
-                param[0][1][:][:] = torch.Tensor([[0.2, 0.2, 0.2],[0.2, 0.1, 0.2],[0.2, 0.2, 0.2]]) # on the preivous state (1:latent) of the grid and its neighbors
-                param[0][2][:][:] = torch.Tensor([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]]) # on the preivous state (2:fire) of the grid and its neighbors
 
-                # Probability to be latent state (output: 1) depends
-                param[1][0][:][:] = torch.Tensor([[0.1, 0.1, 0.1],[0.1, 0.1, 0.1],[0.1, 0.1, 0.1]]) # on the preivous state (0:normal) of the grid and its neighbors
-                param[1][1][:][:] = torch.Tensor([[0.5, 0.5, 0.5],[0.5, 0.9, 0.5],[0.5, 0.5, 0.5]]) # on the preivous state (1:latent) of the grid and its neighbors
-                param[1][2][:][:] = torch.Tensor([[0.2, 0.2, 0.2],[0.2, 0.1, 0.2],[0.2, 0.2, 0.2]]) # on the preivous state (2:fire) of the grid and its neighbors
+                # Probability to be normal state (output: 0) depends on the preivous state of the grid and its neighbors
+                ALPHA0 = 0.999
+                BETA0 = 0.1
+                GAMMA0 = 0.01
+                param[0][0][:][:] = torch.Tensor([[GAMMA0*ALPHA0, GAMMA0*ALPHA0, GAMMA0*ALPHA0],[GAMMA0*ALPHA0, ALPHA0, GAMMA0*ALPHA0],[GAMMA0*ALPHA0, GAMMA0*ALPHA0, GAMMA0*ALPHA0]])
+                param[0][1][:][:] = torch.Tensor([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
+                param[0][2][:][:] = torch.Tensor([[GAMMA0*BETA0, GAMMA0*BETA0, GAMMA0*BETA0],[GAMMA0*BETA0, BETA0, GAMMA0*BETA0],[GAMMA0*BETA0, GAMMA0*BETA0, GAMMA0*BETA0]])
+                
+                # Probability to be latent state (output: 1)
+                ALPHA0 = 0.99
+                BETA0 = 0.1
+                GAMMA0 = 0.001
+                param[1][0][:][:] = torch.Tensor([[GAMMA0*BETA0, GAMMA0*BETA0, GAMMA0*BETA0],[GAMMA0*BETA0, BETA0, GAMMA0*BETA0],[GAMMA0*BETA0, GAMMA0*BETA0, GAMMA0*BETA0]])
+                param[1][1][:][:] = torch.Tensor([[GAMMA0*ALPHA0, GAMMA0*ALPHA0, GAMMA0*ALPHA0],[GAMMA0*ALPHA0, ALPHA0, GAMMA0*ALPHA0],[GAMMA0*ALPHA0, GAMMA0*ALPHA0, GAMMA0*ALPHA0]])
+                param[1][2][:][:] = torch.Tensor([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
 
-                # Probability to be latent state (output: 1) depends
-                param[2][0][:][:] = torch.Tensor([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]]) # on the preivous state (0:normal) of the grid and its neighbors
-                param[2][1][:][:] = torch.Tensor([[0.1, 0.1, 0.1],[0.1, 0.1, 0.1],[0.1, 0.1, 0.1]]) # on the preivous state (1:latent) of the grid and its neighbors
-                param[2][2][:][:] = torch.Tensor([[0.5, 0.5, 0.5],[0.5, 0.9, 0.5],[0.5, 0.5, 0.5]]) # on the preivous state (2:fire) of the grid and its neighbors
+                # Probability to be fire state (output: 2)
+                ALPHA0 = 0.9
+                BETA0 = 0.1
+                GAMMA0 = 0.01
+                param[2][0][:][:] = torch.Tensor([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
+                param[2][1][:][:] = torch.Tensor([[GAMMA0*BETA0, GAMMA0*BETA0, GAMMA0*BETA0],[GAMMA0*BETA0, BETA0, GAMMA0*BETA0],[GAMMA0*BETA0, GAMMA0*BETA0, GAMMA0*BETA0]])
+                param[2][2][:][:] = torch.Tensor([[GAMMA0*ALPHA0, GAMMA0*ALPHA0, GAMMA0*ALPHA0],[GAMMA0*ALPHA0, ALPHA0, GAMMA0*ALPHA0],[GAMMA0*ALPHA0, GAMMA0*ALPHA0, GAMMA0*ALPHA0]])
 
             if name == 'bias':
                 print(name)
-                param.data = torch.ones_like(param.data)*0.05
+                param.data = torch.ones_like(param.data)*0
 
-
-    def reset(self):
-        ### Initialize the state variable values ###
-        self.prob_dist[0][:][:] = 0.9
-        self.prob_dist[1][:][:] = 0.1
-        self.prob_dist[2][:][:] = 0.01
-
-        prob_to_sample = self.prob_dist.clone().detach()
-        prob_to_sample = prob_to_sample.permute(1, 2, 0)
-        print(prob_to_sample.size())
-        sampler = Categorical(torch.tensor(prob_to_sample.view(-1, 3)))
+    def sample_gridmap_from_fire_dist(self, fire_dist):
+        '''
+        fire_dist : (3 x Width X Height)
+        '''
+        fire_dist = fire_dist.clone().detach().permute(1, 2, 0)
+        sampler = Categorical(fire_dist.reshape(-1, 3))
         grid_map = sampler.sample()
-        print(grid_map)
-
-        '''
-
-        print(self.prob_dist.size())
-
-        sampler = Categorical(torch.tensor(self.prob_dist.view(-1, 3).detach()))
-        grid_map = sampler.sample()
-
-        print(self.prob_dist.view(-1, 3))
-
-        print(grid_map)
-        '''
-         
-
-        '''
-        N_fire_starts = 3 # Choose three grid points to set latent fire randomly.
-        latent_fire_i_s = np.random.randint(self.map_width, size=(N_fire_starts))
-        latent_fire_j_s = np.random.randint(self.map_height, size=(N_fire_starts))
-        for i, j in zip(latent_fire_i_s, latent_fire_j_s):
-            self.grid_tensor[2][i][j] = 1
-        '''
-
-    def step(self):
-        prob_tensor = torch.unsqueeze(self.prob_dist, 0).to(DEVICE)
-        prob_tensor = self.transition(prob_tensor).squeeze()
-        prob_tensor = prob_tensor / torch.sum(prob_tensor, 0)
-        self.prob_dist = prob_tensor
-
-        return prob_tensor
+        grid_map = grid_map.reshape(self.map_width, self.map_width, 1)
+        onehot_grid_map = torch.FloatTensor(self.map_width, self.map_height, 3).to(DEVICE)
+        onehot_grid_map.zero_()
+        onehot_grid_map.scatter_(2, grid_map, 1)
+        return onehot_grid_map.permute(2, 0, 1)
 
     def render(self):
-        
-        img = self.grid_tensor.data.cpu().numpy().squeeze().transpose((1,2,0))
+        img = self.realization_state.data.permute(1,2,0).cpu().numpy().squeeze()
         print(img.shape)
         scale = 20
         dim = (self.map_width*scale, self.map_height*scale)
         img_resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-
-        
         cv2.imshow("image", img_resized)
-        cv2.waitKey(1000)
+        cv2.waitKey(100)
 
-        '''
-        img = np.zeros([self.map_width, self.map_height,3])
-        img[:,:,2] = self.grid_tensor.data.cpu().numpy()   #Blue, Green, Red
-        img[:,:,1] = self.grid_tensor.data.cpu().numpy()   #Blue, Green, Red
-        scale = 20
-        dim = (self.map_width*scale, self.map_height*scale)
-        img_resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    def reset(self):
+        ### Resample using the initial distribution ###
+        self.realization_state = self.sample_gridmap_from_fire_dist(self.init_prob_dist)
+        return self.realization_state
 
+
+    def step(self):
+        state = torch.unsqueeze(self.realization_state, 0).to(DEVICE)
+        print(state)
+        prob_dist = self.transition(state).squeeze()
+        print(prob_dist)
+        prob_dist = prob_dist / torch.sum(prob_dist, 0)
+        print(prob_dist)
+        prob_to_sample = prob_dist.clone().detach()
+        self.realization_state = self.sample_gridmap_from_fire_dist(prob_to_sample)
+        return self.realization_state
+
+    
         
-        cv2.imshow("image", img_resized)
-        cv2.waitKey(1000)
-        '''
+        
 
         
 
@@ -128,16 +120,10 @@ class FireMap:
 
 if __name__ == "__main__":
     env = FireMap()
-    env.reset()
-    #print(env.prob_dist)
-
-    #env.step()
-    #print(env.prob_dist)
-    
-    #env.reset()
-    #env.render()
-    #print(env.grid_tensor)
-    #env.step()
-    #print(env.grid_tensor)
-    #env.render()
-    
+    onehot_grid_map = env.reset()
+    #print(onehot_grid_map)
+    env.render()
+    for i in range(100):    
+        onehot_grid_map = env.step()
+        #print(onehot_grid_map)
+        env.render()
